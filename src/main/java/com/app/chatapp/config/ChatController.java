1c90d6a92bd25951241551cf0dc6154ea31593af
Nishu Kumari <sunshine152018@gmail.com>
2025-11-14 05:02:04 +0530

    a real time chat app

---
+++ src/main/java/com/app/chatapp/config/ChatController.java
@@ -0,0 +1,24 @@
+package com.app.chatapp.config;
+
+import com.app.chatapp.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    // when any message received at endpoint "/app/sendMessage" ,
+    // broadcast to "/topic/messages"
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
+
+    @GetMapping("chat")
+    public String chat() {
+        return "chat";
+    }
+}
---
+++ src/main/java/com/app/chatapp/model/ChatMessage.java
@@ -0,0 +1,13 @@
+package com.app.chatapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
+}
